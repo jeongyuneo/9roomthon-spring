32f60a68d0d57dfdcb4823e9a66bcc0b25267f69
piq2255 <piq2255@nate.com>
2023-07-05 14:01:05 +0900

    fix: MemberService import 추가

--- src/main/java/com/example/kakao/MemberService.java
+++ src/main/java/com/example/kakao/MemberService.java
@@ -2,6 +2,8 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import com.hanssarang.backend.member.domain.Member;
 import com.hanssarang.backend.member.domain.MemberRepository;
+import java.util.*;
+import java.util.stream.toList;
 
 @RequiredArgsConstructor
 @Service
